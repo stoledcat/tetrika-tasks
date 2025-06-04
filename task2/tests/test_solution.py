ee77f337bc157b9548865dfda53c826b87eeaa1e
stoledcat <jetnavo@yandex.ru>
2025-06-04 19:21:31 +0300

    Правка теста

--- task2/tests/test_solution.py
+++ task2/tests/test_solution.py
@@ -1,10 +1,9 @@
 def test_main():
     eng_letters = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz"
 
-    file = open("beasts.csv", "r", newline="", encoding="UTF-8")
-    reader = file.read()
+    with open("beasts.csv", "r", newline="", encoding="UTF-8") as file:
+        reader = file.read()
 
     for letter in eng_letters:
         if letter in reader:
             raise ValueError("В файле находится английский символ")
-    file.close()
